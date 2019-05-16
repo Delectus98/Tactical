75c45e02060d61d1c04f0251331bad7438991179
MphDevelopment <theo.jammes@gmail.com>
2019-05-16 16:31:13 +0200

    added methods to Player

--- src/app/Player.java
+++ src/app/Player.java
@@ -1,11 +1,23 @@
 package app;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Player
 {
     protected String name;
     protected List<Unite> unites;
+
+    public Player(String name)
+    {
+        this.name = name;
+        this.unites = new ArrayList<>();
+    }
+
+    public void addUnite(Unite unite)
+    {
+        this.unites.add(unite);
+    }
 
     public String getName()
     {
