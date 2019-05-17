43179a6b6cd26b11c7cd8ee7dcab1153e82198e1
Maxtwist35 <beuchermaxime@laposte.net>
2019-05-17 15:50:28 +0200

    Tile methods creation.

--- src/app/map/Tile.java
+++ src/app/map/Tile.java
@@ -15,16 +15,27 @@ public class Tile
     public Tile(Texture texture, FloatRect rect) //sans struct <=> !isObstacle
     {
         //todo
+        floor = new Sprite(texture);
+        floor.setTextureRect(rect.l, rect.t, rect.w, rect.h);
+
+        isObstacle = false;
     }
 
     public Tile(Texture floor, FloatRect rectFloor, Texture struct, FloatRect rectStruct, boolean isObstacle)
     {
+        this.floor = new Sprite(floor);
+        this.floor.setTextureRect(rectFloor.l, rectFloor.t, rectFloor.w, rectFloor.h);
 
+        this.struct = new Sprite(struct);
+        this.struct.setTextureRect(rectStruct.l, rectStruct.t, rectStruct.w, rectStruct.h);
+
+        this.isObstacle = isObstacle;
     }
 
     public void draw(RenderTarget target)
     {
-
+        if (floor != null) target.draw(floor);
+        if (struct != null) target.draw(struct);
     }
 
     public Sprite getFloor()
