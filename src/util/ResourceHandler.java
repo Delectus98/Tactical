3d3aa2c8ade9ec268ca5b8daaa8e9d44f2cad55e
Maxtwist35 <beuchermaxime@laposte.net>
2019-05-23 22:24:59 +0200

    ResourceHandle improvement and shader support added

--- src/util/ResourceHandler.java
+++ src/util/ResourceHandler.java
@@ -1,7 +1,9 @@
 package util;
 
 
+import Graphics.ConstShader;
 import Graphics.ConstTexture;
+import Graphics.Shader;
 import Graphics.Texture;
 
 import java.io.IOException;
@@ -12,15 +14,36 @@ public class ResourceHandler {
     private ResourceHandler(){}
 
     private static Map<String, Texture> textures = new HashMap<>();
+    private static Map<String, Shader> shaders = new HashMap<>();
 
-    public static ConstTexture loadTexture(String file) throws IOException {
-        return textures.put(file, new Texture(file));
+    public static ConstTexture loadTexture(String file, String name) throws IOException {
+        ConstTexture t = textures.get(file);
+        if (t == null) {
+            Texture tmp = new Texture(file);
+            t = tmp;
+            textures.put(name, tmp);
+        }
+        return t;
     }
 
     public static ConstTexture getTexture(String name){
         return textures.get(name);
     }
 
+
+    public static ConstShader loadShader(String vertex, String fragment, String name) throws IOException {
+        ConstShader s = shaders.get(name);
+        if (s == null) {
+            Shader tmp = new Shader(vertex, fragment);
+            s = tmp;
+            shaders.put(name, tmp);
+        }
+        return s;
+    }
+
+    public static ConstShader getShader(String name) {
+        return shaders.get(name);
+    }
 /*
     public static ConstSound loadSound(String sound) throws IOException {}
 */
