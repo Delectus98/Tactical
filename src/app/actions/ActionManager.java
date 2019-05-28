4353e92eca148251ea84fca174d08d68d9f01b55
Maxtwist35 <beuchermaxime@laposte.net>
2019-05-28 15:40:42 +0200

    Doc update

--- src/app/actions/ActionManager.java
+++ src/app/actions/ActionManager.java
@@ -12,25 +12,58 @@ public abstract class ActionManager {
     protected Unite unite;
     protected Game game;
 
+    /**
+     * Creates an Action Manager
+     * @param user launcher
+     * @param game context
+     */
     public ActionManager(Unite user, Game game)
     {
         this.unite = user;
         this.game = game;
     }
 
+    /**
+     * Updates preparation
+     * @param time elapsed time since last frame
+     */
     public abstract void updatePreparation(ConstTime time); // met a jour la phase préparation où le joueur choisit encore comment elle va se faire
 
+    /**
+     * Draw stuff above floor (on map)
+     * @param target render target
+     */
     public abstract void drawAboveFloor(RenderTarget target);
+    /**
+     * Draw stuff above struct (on map)
+     * @param target render target
+     */
     public abstract void drawAboveStruct(RenderTarget target);
+    /**
+     * Draw stuff above entity (on map)
+     * @param target render target
+     */
     public abstract void drawAboveEntity(RenderTarget target);
+    /**
+     * Draw stuff above hud (on hud)
+     * @param target render target
+     */
     public abstract void drawAboveHUD(RenderTarget target);
 
+    /**
+     * Gives the cost of the action
+     * @return the cost of the action
+     */
     public abstract int getCost(); // renvoie le cout de l'action en PA
 
+    /**
+     * Checks if the preparation is finished and the built action possible
+     * @return true if the preparation is finished and the built action possible else false.
+     */
     public abstract boolean isAvailable(); // renvoie vrai si l'action est possible/disponible <=> renvoie vrai si la préparation est possible
     /**
-     * Construit le déroulement d'une action
-     * @return
+     * Build an action corresponding to action manager
+     * @return an action corresponding to action manager
      */
     public abstract Action build();
 }
