c3b04abf0ff456ac3fc770c5eb77003f9840eb19
Maxtwist35 <beuchermaxime@laposte.net>
2019-05-28 15:41:42 +0200

    Doc update

--- src/app/map/Map.java
+++ src/app/map/Map.java
@@ -5,24 +5,41 @@ import Graphics.Vector2i;
 import java.util.Collection;
 import java.util.List;
 
-//TODO Remove draw features to let Game draw itself
+
 public abstract class Map
 {
     protected List<Vector2i>[] spawnPoints;
 
     protected Tile[][] world;
 
-    //TODO (removed?) public abstract void drawFloor(int x, int y, int x2, int y2, RenderTarget target);
-
-    //TODO (removed?) public abstract void drawStruct(int x, int y, int x2, int y2, RenderTarget target);
-
+    /**
+     * Sets a default spawn points for a specific player
+     * @param spawnPoints list of available tiles
+     * @param player specified player
+     */
     public abstract void setSpawnPoints(Collection<Vector2i> spawnPoints, int player);
 
+    /**
+     * Gives the world tiles
+     * @return the world tiles
+     */
     public abstract Tile[][] getWorld();
 
+    /**
+     * Gives the width of the world in tiles
+     * @return the width of the world in tiles
+     */
     public abstract int getWidth();
-
+    /**
+     * Gives the height of the world in tiles
+     * @return the height of the world in tiles
+     */
     public abstract int getHeight();
 
+    /**
+     * Gives a list of available spawn points for a specific player
+     * @param currentPlayer specified player
+     * @return a list of available spawn points for a specific player
+     */
     public abstract List<Vector2i> getSpawnPoints(int currentPlayer);
 }
