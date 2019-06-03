2642671526c6d599de9d644b1ae8507177d131ee
Maxtwist35 <beuchermaxime@laposte.net>
2019-06-03 23:02:24 +0200

    Resume to weapon selection

--- src/app/actions/ShootingManager.java
+++ src/app/actions/ShootingManager.java
@@ -27,11 +27,14 @@ public class ShootingManager extends ActionManager {
     private Vector2i p2 = null;
     private GameInput input = null;
     private boolean selectMode = true;
-    // hud
+    // hud select weapon
     private RectangleShape hudRect;
     private Text fstWeapon;
     private Text sndWeapon;
     private Text thdWeapon;
+    // hud back to selection
+    private Text resume;
+    private RectangleShape resumeBox;
 
     public ShootingManager(Unite user, Game game, GameInput input) {
         super(user, game);
@@ -60,6 +63,10 @@ public class ShootingManager extends ActionManager {
         fstWeapon = new Text(ResourceHandler.getFont("default"), /*super.unite.getPrimary().toString()*/"1. First Weapon");
         sndWeapon = new Text(ResourceHandler.getFont("default"), /*super.unite.getPrimary().toString()*/"2. Second Weapon");
         thdWeapon = new Text(ResourceHandler.getFont("default"), /*super.unite.getPrimary().toString()*/"3. Third Weapon");
+        resume = new Text(ResourceHandler.getFont("default"), /*super.unite.getPrimary().toString()*/"Resume Selection");
+        resume.setPosition(20, 50);
+        resumeBox = new RectangleShape(20, 50, resume.getBounds().w, resume.getBounds().h);
+        resumeBox.setFillColor(new Color(0.5f,0.5f,0.5f,0.9f));
     }
 
     @Override
@@ -68,9 +75,13 @@ public class ShootingManager extends ActionManager {
         if (selectMode) {
             selectMode = false;
 
-            fstWeapon.setPosition(input.getFrameRectangle().w / 2.f - 100, input.getFrameRectangle().h / 2.f - 100);
-            sndWeapon.setPosition(input.getFrameRectangle().w / 2.f - 100, input.getFrameRectangle().h / 2.f);
-            thdWeapon.setPosition(input.getFrameRectangle().w / 2.f - 100, input.getFrameRectangle().h / 2.f + 100);
+            hudRect.setPosition(input.getFrameRectangle().w / 2.f - hudRect.getBounds().w /2.f, input.getFrameRectangle().h / 2.f - hudRect.getBounds().h /2.f);
+            fstWeapon.setPosition(input.getFrameRectangle().w / 2.f - fstWeapon.getBounds().w /2.f, input.getFrameRectangle().h / 2.f - 100);
+            sndWeapon.setPosition(input.getFrameRectangle().w / 2.f - sndWeapon.getBounds().w /2.f, input.getFrameRectangle().h / 2.f);
+            thdWeapon.setPosition(input.getFrameRectangle().w / 2.f - thdWeapon.getBounds().w /2.f, input.getFrameRectangle().h / 2.f + 100);
+            fstWeapon.setFillColor(Color.White);
+            sndWeapon.setFillColor(Color.White);
+            thdWeapon.setFillColor(Color.White);
 
             if (input.getKeyboard().isKeyPressed(AZERTYLayout.PAD_1.getKeyID()) || input.getKeyboard().isKeyPressed(AZERTYLayout.NUM_1.getKeyID())) {
                 selectedWeapon = super.unite.getPrimary();
@@ -93,10 +104,16 @@ public class ShootingManager extends ActionManager {
 
             if (fstWeapon.getBounds().contains(input.getMousePositionOnHUD().x, input.getMousePositionOnHUD().y)) {
                 fstWeapon.move(10,0);
+                if (input.isLeftPressed()) fstWeapon.setFillColor(Color.Yellow);
+                else fstWeapon.setFillColor(Color.Green);
             } else if (sndWeapon.getBounds().contains(input.getMousePositionOnHUD().x, input.getMousePositionOnHUD().y)) {
                 sndWeapon.move(10,0);
+                if (input.isLeftPressed()) sndWeapon.setFillColor(Color.Yellow);
+                else sndWeapon.setFillColor(Color.Green);
             } else if (thdWeapon.getBounds().contains(input.getMousePositionOnHUD().x, input.getMousePositionOnHUD().y)) {
                 thdWeapon.move(10,0);
+                if (input.isLeftPressed()) thdWeapon.setFillColor(Color.Yellow);
+                else thdWeapon.setFillColor(Color.Green);
             }
 
             hudRect.setPosition(input.getFrameRectangle().w / 2.f, input.getFrameRectangle().h / 2.f);
@@ -119,8 +136,18 @@ public class ShootingManager extends ActionManager {
 
                     Vector2i tile = new Vector2i(mouse.mul(1.f / 64.f));
 
+                    if (resume.getBounds().contains(input.getMousePositionOnHUD().x, input.getMousePositionOnHUD().y)) {
+                        if (input.isLeftPressed()) resume.setFillColor(Color.Yellow);
+                        else resume.setFillColor(Color.Green);
+                    } else {
+                        resume.setFillColor(Color.White);
+                    }
+
                     if (input.isLeftReleased()) {
-                        if (!tile.equals(p1) && selectable.contains(tile)) {
+
+                        if (resume.getBounds().contains(input.getMousePositionOnHUD().x, input.getMousePositionOnHUD().y)) {
+                            selectMode = true;
+                        } else if (!tile.equals(p1) && selectable.contains(tile)) {
                             p2 = tile;
                             selected.setFillColor(Color.White);
                             selected.setPosition(tile.x * 64, tile.y * 64);
@@ -168,6 +195,8 @@ public class ShootingManager extends ActionManager {
         if (selectMode) target.draw(thdWeapon);
         if (selectMode) target.draw(fstWeapon);
         if (selectMode) target.draw(sndWeapon);
+        if (!selectMode) target.draw(resumeBox);
+        if (!selectMode) target.draw(resume);
     }
 
     @Override
