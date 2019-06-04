deeea822c91222ceed8aad3890428e41bcaa682b
Maxtwist35 <beuchermaxime@laposte.net>
2019-06-04 18:02:10 +0200

    Add data types to registration

--- src/app/network/GameRegistration.java
+++ src/app/network/GameRegistration.java
@@ -3,6 +3,9 @@ package app.network;
 import Graphics.Vector2f;
 import Graphics.Vector2i;
 import app.actions.Shooting;
+import app.weapon.BladeProjectile;
+import app.weapon.BulletProjectile;
+import app.weapon.BlastingProjectile;
 import app.weapon.Impact;
 import com.esotericsoftware.kryonet.EndPoint;
 
@@ -19,9 +22,13 @@ public class GameRegistration extends PacketRegistration {
         point.getKryo().register(ActionPacket.class);
 
         //data
+        point.getKryo().register(String.class);
         point.getKryo().register(Vector2i.class);
         point.getKryo().register(Vector2f.class);
         point.getKryo().register(Impact.class);
+        point.getKryo().register(BulletProjectile.class);
+        point.getKryo().register(BladeProjectile.class);
+        point.getKryo().register(BlastingProjectile.class);
 
         //actions
         point.getKryo().register(Shooting.class);
