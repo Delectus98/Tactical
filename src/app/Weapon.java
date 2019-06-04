69115de1c087c06db7f6c8b4828f5ba390496f33
Maxtwist35 <beuchermaxime@laposte.net>
2019-06-04 17:53:10 +0200

    Projectile can be/is built by each weapon

--- src/app/Weapon.java
+++ src/app/Weapon.java
@@ -6,37 +6,39 @@ import Graphics.FloatRect;
 import Graphics.Vector2i;
 import app.map.Map;
 import app.weapon.Impact;
+import app.weapon.Projectile;
 
 import java.util.List;
 
 public abstract class Weapon
 {
-
-    public static class WeaponInfo {
-        private String texture;
-        private FloatRect rect;
-    }
-
-    public static class ProjectileInfo {
-        private String texture;
-        private List<FloatRect> spriteAnimation;
-        private ConstTime frameDuration;
-        //private Trajectory trajectory;
-
-        private float radian;
-    }
-
+    /**
+     * Checks if a weapon can shoot in a its own specific range
+     * @param distance between unite that shoot and tile where the target is
+     * @return true if a weapon can shoot in a its own specific range else false
+     */
     public abstract boolean isInRange(float distance);
 
+    /**
+     * Gives the cost of one use
+     * @return the cost of one use
+     */
     public abstract int getCost(); //negatif = vide les PA si déjà déplacé; sinon val absolue
 
+    /**
+     *
+     * @return
+     */
     public abstract int getAmmunition(); //-1 = infini, sinon utile pour armes speciales
 
     public abstract float getAccuracy(float distance);
 
+    public abstract Projectile buildProjectile();
+
     public abstract Impact getImpactZone(Vector2i target, Map map);
 
     public abstract Sprite getSprite();
+
     //public abstract ProjectileInfo ...
 
     //public abstract WeaponInfo ...
