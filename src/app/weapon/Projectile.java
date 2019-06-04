38f7bb5882116bfadc3a41ca38a04b1b0abf4a6a
Maxtwist35 <beuchermaxime@laposte.net>
2019-06-04 17:56:25 +0200

    default target/thrower position & Doc

--- src/app/weapon/Projectile.java
+++ src/app/weapon/Projectile.java
@@ -3,13 +3,52 @@ package app.weapon;
 import System.*;
 import Graphics.*;
 
+/**
+ * Represents the graphics component shown when a Weapon is used
+ */
 public abstract class Projectile {
 
+    protected Vector2f firstPos;
+    protected Vector2f lastPos;
+
+    /**
+     * Must be initialized before used
+     */
     public abstract void init();
 
-    public abstract boolean isTerminated();
+    /**
+     * Sets the position of the unite that throw the projectile
+     * @param pos thrower position
+     */
+    public final void setTrackerPosition(Vector2f pos)
+    {
+        firstPos = pos;
+    }
 
+    /**
+     * Sets the position of the tile that is targeted
+     * @param pos target position
+     */
+    public final void setTargetPosition(Vector2f pos)
+    {
+        lastPos = pos;
+    }
+
+    /**
+     * Checks if projectile has finished hitting (animation, trajectory, etc...)
+     * @return true if projectile has finished hitting (animation, trajectory, etc...) else false
+     */
+    public abstract boolean hasFinishedHitting();
+
+    /**
+     * Updates projectile specific stuff (as animation, trajectory, explosion...)
+     * @param time
+     */
     public abstract void update(ConstTime time);
 
+    /**
+     * Draws graphics components
+     * @param target render target
+     */
     public abstract void draw(RenderTarget target);
 }
