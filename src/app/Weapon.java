0cfe92774f3c6df4419e8c0d82cf557421c13b5b
Maxtwist35 <beuchermaxime@laposte.net>
2019-06-06 15:30:21 +0200

    Documentation update

--- src/app/Weapon.java
+++ src/app/Weapon.java
@@ -26,20 +26,35 @@ public abstract class Weapon
     public abstract int getCost(); //negatif = vide les PA si déjà déplacé; sinon val absolue
 
     /**
-     *
-     * @return
+     * Gives the total remaining ammunition of the weapon.
+     * @return the total remaining ammunition of the weapon.
      */
     public abstract int getAmmunition(); //-1 = infini, sinon utile pour armes speciales
 
+    /**
+     * Gives the chance of a weapon to touch the enemy.
+     * Accuracy must be in [0,1] range
+     */
     public abstract float getAccuracy(float distance);
 
+    /**
+     * Creates a specific weapon projectile that will be drawn and updated.
+     * @return a specific weapon projectile that will be drawn and updated.
+     */
     public abstract Projectile buildProjectile();
 
+    /**
+     * Gives an interface that describe the damages inflected on an world map.
+     * @param thrower unite that use the weapon
+     * @param target tile where thrower is pointing to.
+     * @param map world map.
+     * @return impact of the weapon on the map.
+     */
     public abstract Impact getImpactZone(Vector2i thrower, Vector2i target, Map map);
 
+    /**
+     * Gives the graphic component of the weapon.
+     * @return the graphic component of the weapon.
+     */
     public abstract Sprite getSprite();
-
-    //public abstract ProjectileInfo ...
-
-    //public abstract WeaponInfo ...
 }
