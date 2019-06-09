b7029f1711f5b1888184a3ea6037d171ebe2a698
Maxtwist35 <beuchermaxime@laposte.net>
2019-06-09 17:37:52 +0200

    Weapon update

--- src/app/weapon/CombatRifle.java
+++ src/app/weapon/CombatRifle.java
@@ -1,12 +1,17 @@
 package app.weapon;
 
+import Graphics.FloatRect;
 import Graphics.Sprite;
+import Graphics.Vector2f;
 import Graphics.Vector2i;
 import app.Weapon;
 import app.map.Map;
 
 
 public class CombatRifle extends Weapon {
+    private static String weaponSprite = "";
+    private static FloatRect weaponRect;
+
     private Sprite sprite;
 
     @Override
@@ -30,14 +35,13 @@ public class CombatRifle extends Weapon {
     }
 
     @Override
-    public Projectile buildProjectile() {
-        return new BulletProjectile();
+    public Projectile buildProjectile(Vector2i thrower, Vector2i target) {
+        return new BulletProjectile(weaponSprite, weaponRect, new Vector2f(thrower.x*64.f, thrower.y*64.f), new Vector2f(target.x*64.f, target.y*64.f));
     }
 
     @Override
     public Impact getImpactZone(Vector2i thrower, Vector2i target, Map map) {
         Impact i = new Impact();
-        i.add(target, 5);
         i.add(target, 5);
         return i;
     }
