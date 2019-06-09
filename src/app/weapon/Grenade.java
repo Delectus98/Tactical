2c0cef5d60f67441ed9c9b73ea58c563c5070bdb
Maxtwist35 <beuchermaxime@laposte.net>
2019-06-09 17:37:52 +0200

    Weapon update

--- src/app/weapon/Grenade.java
+++ src/app/weapon/Grenade.java
@@ -1,6 +1,8 @@
 package app.weapon;
 
+import Graphics.FloatRect;
 import Graphics.Sprite;
+import Graphics.Vector2f;
 import Graphics.Vector2i;
 import app.Weapon;
 import app.map.Map;
@@ -8,8 +10,12 @@ import util.MapUtil;
 
 
 public class Grenade extends Weapon {
+    private static String weaponSprite = "";
+    private static FloatRect weaponRect;
+
     private int ammunition = 3;
     private Sprite sprite;
+    private int radius = 5;
 
     @Override
     public boolean isInRange(float distance) {
@@ -32,19 +38,18 @@ public class Grenade extends Weapon {
     }
 
     @Override
-    public Projectile buildProjectile() {
+    public Projectile buildProjectile(Vector2i thrower, Vector2i target) {
         ammunition--;
-        return new BlastingProjectile();
+        return new BlastingProjectile(weaponSprite, weaponRect, new Vector2f(thrower.x*64.f, thrower.y*64.f), new Vector2f(target.x*64.f, target.y*64.f), 1);
     }
 
     @Override
     public Impact getImpactZone(Vector2i thrower, Vector2i target, Map map) {
-        Impact impact = null;
+        Impact i = new Impact();
 
-        // add radius if not wall
-        //impact = new Impact(MapUtil.getVisibles())
+        MapUtil.getVisibles(target, radius, map).forEach(t -> i.add(t, radius + 1 - (int)(new Vector2f(target).sum(new Vector2f(t).neg()).length())));
 
-        return impact;
+        return i;
     }
 
     @Override
