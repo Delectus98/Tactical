df3f68850581477f6de1d0c376f32de52ab4f1d2
MphDevelopment <theo.jammes@gmail.com>
2019-06-11 17:37:41 +0200

    updated HudUnite WIP

--- src/app/hud/HudUnite.java
+++ src/app/hud/HudUnite.java
@@ -29,12 +29,18 @@ public class HudUnite
     private boolean lowered;
     private Sprite lower;
 
+    private boolean clicked;
+
     //todo more actions for later
     //better graphics
-    private Text tirer;
     private ActionManager selectedAction;
 
-    private ActionManager tir;
+    private ShootingManager tir1;
+    private Text tirer1;
+    private ShootingManager tir2;
+    private Text tirer2;
+    private ShootingManager tir3;
+    private Text tirer3;
     private ActionManager move;
 
     public HudUnite(Player player, Unite unite, GameInput input, Game game)
@@ -44,9 +50,14 @@ public class HudUnite
         this.input = input;
         this.game = game;
 
-        tir = new ShootingManager(player, selected, game, input);
-        move = new MovingManager(player, selected, game, input);
-        selectedAction = move;
+        if (player.getUnites().contains(selected))
+        {
+            tir1 = new ShootingManager(player, selected, selected.getPrimary(), game, input);
+            tir2 = new ShootingManager(player, selected, selected.getSecondary(), game, input);
+            tir3 = new ShootingManager(player, selected, selected.getMelee(), game, input);
+            move = new MovingManager(player, selected, game, input);
+            selectedAction = move;
+        }
 
         try
         {
@@ -65,8 +76,14 @@ public class HudUnite
             pa = new Text(ResourceHandler.getFont("default"), "PA: " + unite.getSparePoints());
             pa.setPosition(hp.getPosition().x, hp.getPosition().y + 25);
 
-            tirer = new Text(ResourceHandler.getFont("default"), "A: TIRER");
-            tirer.setPosition(hp.getPosition().x + 64, hp.getPosition().y);
+            tirer1 = new Text(ResourceHandler.getFont("default"), "1: TIRER");
+            tirer1.setPosition(hp.getPosition().x + 64, hp.getPosition().y);
+
+            tirer2 = new Text(ResourceHandler.getFont("default"), "2:TIRER");
+            tirer2.setPosition(tirer1.getPosition().x, tirer1.getPosition().y + 64);
+
+            tirer3 = new Text(ResourceHandler.getFont("default"), "3:TIRER");
+            tirer3.setPosition(tirer2.getPosition().x, tirer2.getPosition().y + 64);
 
         } catch (IOException e0)
         {
@@ -87,12 +104,26 @@ public class HudUnite
                 target.draw(pa);
                 if (player.getUnites().contains(selected))
                 {
-                    if (tir.isAvailable())
+                    if (tir1.isAvailable())
                     {
-                        target.draw(tirer);
-                    }else
+                        target.draw(tirer1);
+                    } else
                     {
-                        target.draw(tirer, ResourceHandler.getShader("grey"));
+                        target.draw(tirer1, ResourceHandler.getShader("grey"));
+                    }
+                    if (tir2.isAvailable())
+                    {
+                        target.draw(tirer2);
+                    } else
+                    {
+                        target.draw(tirer2, ResourceHandler.getShader("grey"));
+                    }
+                    if (tir3.isAvailable())
+                    {
+                        target.draw(tirer3);
+                    } else
+                    {
+                        target.draw(tirer3, ResourceHandler.getShader("grey"));
                     }
                 }
             }
@@ -109,13 +140,28 @@ public class HudUnite
             if (lower.getBounds().contains(input.getMousePositionOnHUD().x, input.getMousePositionOnHUD().y))
             {
                 this.lowered = !this.lowered;
-            } else if (tirer.getBounds().contains(input.getMousePositionOnHUD().x, input.getMousePositionOnHUD().y)
-                    && !lowered && tir.isAvailable())
+                clicked = true;
+            } else if (tirer1.getBounds().contains(input.getMousePositionOnHUD().x, input.getMousePositionOnHUD().y)
+                    && !lowered && tir1.isPossible())
             {
                 //on passe en action de tire
-                selectedAction = tir;
+                selectedAction = tir1;
                 //on reduit le hud
                 this.lowered = true;
+                System.out.println("SELECTED TIR1");
+                clicked = true;
+            }else if (tirer2.getBounds().contains(input.getMousePositionOnHUD().x, input.getMousePositionOnHUD().y)
+                    && !lowered && tir2.isPossible())
+            {
+                selectedAction = tir2;
+                this.lowered = true;
+                clicked = true;
+            } else if (tirer3.getBounds().contains(input.getMousePositionOnHUD().x, input.getMousePositionOnHUD().y)
+                    && !lowered && tir3.isPossible())
+            {
+                selectedAction = tir3;
+                this.lowered = true;
+                clicked = true;
             }
         }
     }
@@ -135,5 +181,10 @@ public class HudUnite
     public void resetSelectedAction()
     {
         this.selectedAction = null; //or MovingManager
+    }
+
+    public boolean isClicker()
+    {
+        return clicked;
     }
 }
