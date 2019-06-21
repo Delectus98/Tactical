4f5ca11a06411ec44ff2444b8708b462db33192b
maxjbh <maxjbh@hotmail.com>
2019-06-21 14:57:34 +0200

    code pour la musique

---
+++ src/app/sounds/Music.java
@@ -0,0 +1,90 @@
+package app.sounds;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.sound.sampled.*;
+
+
+/*
+    TODO call updateLoop on every cycle, add nessesary code for a mute button
+ */
+
+public enum Music {
+    SONG("res\\music\\mainMenuMusic.wav"),
+    SONG2("res\\music\\breakingBad.wav"),
+    SONG3("res\\music\\action1.wav"),
+    SONG4("res\\music\\medeval.wav");
+
+    private static ArrayList<Music> pickable = new ArrayList<Music>();
+    private static Music current = null;
+
+    // Each sound effect has its own clip, loaded with its own sound file.
+    private Clip clip;
+
+    // Constructor to construct each element of the enum with its own sound file.
+    Music(String soundFileName) {
+
+        try {
+            File soundFile = new File(soundFileName);
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            clip = AudioSystem.getClip();
+            clip.open(audioIn);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateLoop(){
+        //refill pickables if they have all been played.
+        if(pickable.isEmpty()){
+            pickable.add(Music.SONG);
+            pickable.add(Music.SONG2);
+            pickable.add(Music.SONG3);
+            pickable.add(Music.SONG4);
+        }
+        //pick a new song if need be and remove it from pickables.
+        if(current==null || current.hasEnded()){
+            Random rand = new Random();
+            int index = rand.nextInt(pickable.size());
+            current = pickable.get(index);
+            pickable.remove(current);
+            current.play();
+        }
+
+    }
+
+    private void play() {
+
+        if (clip.isRunning())
+            clip.stop();
+        clip.setFramePosition(0);
+        clip.start();
+
+    }
+    private boolean hasEnded() {
+        return clip.getFramePosition()>=clip.getFrameLength();
+    }
+    private void playIfFinished() {
+
+        if (clip.getFramePosition()>=clip.getFrameLength()) {
+            clip.setFramePosition(0);
+            clip.start(); // Start playing
+
+        }
+
+    }
+    private void stop() {
+        if (clip.isRunning())
+            clip.stop();
+    }
+
+    static void init() {
+        values();
+    }
+}
+
